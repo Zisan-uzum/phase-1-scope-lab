07fc923630fc03d8c29d1f9f7c80a08e5a25213d
zisan <uzummzisann@gmail.com>
2022-04-22 02:42:29 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = "bob";
+const leastFavoriteCustomer = "alice";
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "bob";
+}
+// console.log(bestCustomer);
+//setBestCustomer();
+console.log(bestCustomer);
+// overwriteBestCustomer();
+// console.log(bestCustomer);
+// changeLeastFavoriteCustomer();
